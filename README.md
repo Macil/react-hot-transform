d7a6c8d2467ddf243e695e2772d608420c9c53e2
Chris Cowan <agentme49@gmail.com>
2015-08-24 12:52:57 -0700

    first working version

---
+++ README.md
@@ -0,0 +1,7 @@
+# react-hot-transform
+
+This is an adapter for
+[react-hot-loader](https://github.com/gaearon/react-hot-loader) that is
+compatible with [Browserify-HMR](https://github.com/AgentME/browserify-hmr).
+See Browserify-HMR's Quick Example section for an example of
+react-hot-transform in use.
---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var through = require('through2');
+var path = require('path');
+var convert = require('convert-source-map');
+var reactHotLoader = require('react-hot-loader');
+
+module.exports = function(file, opts) {
+  var basedir = opts.basedir !== undefined ? opts.basedir : process.cwd();
+
+  return through.obj(function(row, enc, next) {
+    var source = row.toString();
+    var inputMapCV = convert.fromSource(source);
+    var inputMap;
+    if (inputMapCV) {
+      inputMap = inputMapCV.toObject();
+      source = convert.removeComments(source);
+    }
+    reactHotLoader.call({
+      resourcePath: file,
+      callback: function(err, source, map) {
+        if (source && map) {
+          source = source + convert.fromObject(map).toComment();
+        }
+        next(err, source);
+      }
+    }, source, inputMap);
+  });
+};
